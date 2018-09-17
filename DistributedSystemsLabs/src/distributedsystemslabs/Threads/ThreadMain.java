c095333c1441438b6fde9cacac5be24e5ea5015c
puser <puser>
2018-09-17 15:02:18 +0100

    Lab1 work

--- DistributedSystemsLabs/src/distributedsystemslabs/Threads/ThreadMain.java
+++ DistributedSystemsLabs/src/distributedsystemslabs/Threads/ThreadMain.java
@@ -5,10 +5,32 @@
  */
 package distributedsystemslabs.Threads;
 
+import java.awt.Dimension;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
 /**
  *
  * @author puser
  */
-public class ThreadMain {
+public class ThreadMain extends JFrame{
+    private JPanel [] panels;
+    private ThreadTimer [] threads;
+    private int[] sleeps = {1,2,5,10};
     
+    public ThreadTimerExample(){
+        this.setTitle("Threaded Clock Example");
+        this.setBounds(100,100,400,200);
+        this.setPreferredSize(new Dimension(400,200));
+        
+        panels = new JPanel[4];
+        threads = new ThreadTimer[4];
+        
+        for(int i = 0; i < 4; i++){
+            panels[i] = new JPanel();
+            JLabel myLabel = new JLabel("test");
+            threads[i] = new ThreadTimer(myLabel, sleeps[i]);
+        }
+    }
 }
--- DistributedSystemsLabs/src/distributedsystemslabs/Threads/ThreadTimer.java
+++ DistributedSystemsLabs/src/distributedsystemslabs/Threads/ThreadTimer.java
@@ -1,14 +1,36 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package distributedsystemslabs.Threads;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import javax.swing.JLabel;
 
 /**
  *
- * @author puser
+ * @author 15155528
  */
-public class ThreadTimer {
+public class ThreadTimer extends Thread {
+    // ALT: implements runnable
+    private JLabel myLabel;
+    private int sleep_interval;
     
+    public ThreadTimer(JLabel p, int t){
+        myLabel = p;
+        sleep_interval = t*1000;
+    }
+    
+   
+    @Override
+    public void run(){
+        while (true) {
+            String timestamp = new SimpleDateFormat("HH:mm:ss").format(new Date());
+            myLabel.setText(timestamp);
+            
+            try {
+                sleep(sleep_interval);
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        }
+    }
 }
+
