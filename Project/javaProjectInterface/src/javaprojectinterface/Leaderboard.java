19533f6fafda3490217d4e600cec5e215fc27e85
aarond203 <15148602@studentmail.ul.ie>
2018-11-25 22:01:28 +0000

    Revert "Revert "Leaderboard thread done""
    This reverts commit 95e21a4468e031ecd2bb1cec5a77289882bef541.

--- Project/javaProjectInterface/src/javaprojectinterface/Leaderboard.java
+++ Project/javaProjectInterface/src/javaprojectinterface/Leaderboard.java
@@ -5,27 +5,83 @@
  */
 package javaprojectinterface;
 
+import TTTWebApplication.TTTWebService;
 import javax.swing.table.*;
 // thread for leaderboard
-// dispose of window upon close
 
 
 /**
  *
- * @author aaron
+ * @author Aaron
  */
 public class Leaderboard extends javax.swing.JFrame {
+    private String leaderTable, newLeaderTable;
+    private String[] results;
+    private String[][] finalLeagueTable;
+    private DefaultTableModel model;
+    public final Runnable refreshLeaderboard;
+    
+    public Leaderboard(TTTWebService proxy,String leagueTable) {
+        refreshLeaderboard = new Runnable() {
+            public void run() {
+                while(proxy.leagueTable() != null && !proxy.leagueTable().equals("ERROR-NOGAMES")) {
+                    //System.out.print("\nThread running");
+                    
+                    newLeaderTable = proxy.leagueTable();
+                    System.out.println(leaderTable);
+                    System.out.println("===============================");
+                    System.out.print("\n" + newLeaderTable);
+                    
+                    leaderTable ="";
+                    
+                    if (!newLeaderTable.equals(leaderTable)) {
+                        leaderTable = newLeaderTable;
+                        System.out.print(", In if statement");
+                        results = newLeaderTable.split("\n");
+                        finalLeagueTable = new String [results.length][];
 
-    /**
-     * Creates new form Leaderboard
-     */
-    public Leaderboard(String leagueTable) {
+                        model = new DefaultTableModel();
+                        model = (DefaultTableModel)leaderboardTable.getModel();
+                        model.setRowCount(0);
+
+                        for (int i = 0; i < results.length; i++) {
+                            System.out.print(", loop" + i);
+                            String[] game = results[i].split(",");
+                            finalLeagueTable[i] = game;
+
+                            //if (!model.equals(finalLeagueTable[i]))
+                                model.addRow(finalLeagueTable[i]);
+                        }
+
+                        /*try {
+                            Thread.currentThread().sleep(5000);
+                        } catch (Exception ex) {
+                            System.out.print(ex);
+                        }*/
+                        //leaderboardTable.setVisible(false);
+                        //leaderboardTable.setVisible(true);
+                    }
+                    
+                    //leaderTable = newLeaderTable;
+                    System.out.print(", Repeat");
+                    
+                    try {
+                        Thread.currentThread().sleep(5000);
+                    } catch (Exception ex) {
+                        System.out.print(ex);
+                    }
+                }
+            }
+        };
+        
         initComponents();
+        /*leaderTable = leagueTable;
+        // System.out.println(leaderTable);
         
-        String[] results = leagueTable.split("\n");
-        String[][] finalLeagueTable = new String [results.length][];
+        results = leaderTable.split("\n");
+        finalLeagueTable = new String [results.length][];
         
-        DefaultTableModel model = new DefaultTableModel();
+        model = new DefaultTableModel();
         model = (DefaultTableModel)leaderboardTable.getModel();
 
         for (int i = 0; i < results.length; i++) {
@@ -33,9 +89,15 @@ public class Leaderboard extends javax.swing.JFrame {
             
             finalLeagueTable[i] = game;
             model.addRow(finalLeagueTable[i]);
-        }
+        }*/
+        startLeaderboardThread();
     }
-
+    
+    public void startLeaderboardThread() {
+        new Thread(this.refreshLeaderboard).start();
+    }
+                    
+    
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
